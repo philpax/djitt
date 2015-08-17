ef6ae60de8f8a1276e9afbe1e592d563cbd9c0c8
Philpax <me@philpax.me>
2015-08-18 09:01:24 +1000

    Fix #4 - use a Reg structure instead of an enum for typesafety

--- source/djitt.d
+++ source/djitt.d
@@ -4,35 +4,78 @@ import std.string;
 import std.array;
 import std.stdio;
 
-enum Register
+struct Reg(int Size)
 {
-    EAX,
-    ECX,
-    EDX,
-    EBX,
-    ESP,
-    EBP,
-    ESI,
-    EDI,
+    ubyte index;
+}
+
+alias Reg8 = Reg!8;
+alias Reg16 = Reg!16;
+alias Reg32 = Reg!32;
+alias Reg64 = Reg!64;
+
+enum AL = Reg8(0);
+enum CL = Reg8(1);
+enum DL = Reg8(2);
+enum BL = Reg8(3);
+enum AH = Reg8(4);
+enum CH = Reg8(5);
+enum DH = Reg8(6);
+enum BH = Reg8(7);
+
+enum AX = Reg16(0);
+enum CX = Reg16(1);
+enum DX = Reg16(2);
+enum BX = Reg16(3);
+enum SP = Reg16(4);
+enum BP = Reg16(5);
+enum SI = Reg16(6);
+enum DI = Reg16(7);
+
+enum EAX = Reg32(0);
+enum ECX = Reg32(1);
+enum EDX = Reg32(2);
+enum EBX = Reg32(3);
+enum ESP = Reg32(4);
+enum EBP = Reg32(5);
+enum ESI = Reg32(6);
+enum EDI = Reg32(7);
+
+enum RAX = Reg64(0);
+enum RCX = Reg64(1);
+enum RDX = Reg64(2);
+enum RBX = Reg64(3);
+enum RSP = Reg64(4);
+enum RBP = Reg64(5);
+enum RSI = Reg64(6);
+enum RDI = Reg64(7);
+
+enum Mode
+{
+    Memory,
+    MemoryOffset8,
+    MemoryOffsetExt,
+    Register
 }
 
 union ModRM
 {
-    static ModRM opCall(Register register1, Register register2, ubyte mod = 3)
+    static ModRM opCall(int Size)(
+        Reg!Size register1, Reg!Size register2, Mode mod = Mode.Register)
     {
         ModRM modRM;
-        modRM.register1 = register1;
-        modRM.register2 = register2;
-        modRM.mod = mod;
+        modRM.register1 = register1.index;
+        modRM.register2 = register2.index;
+        modRM.mod = cast(ubyte)mod;
         return modRM;
     }
 
     struct
     {
         mixin(bitfields!(
-            Register,   "register1",    3,
-            Register,   "register2",    3,
-            ubyte,      "mod",          2,
+            ubyte,  "register1",    3,
+            ubyte,  "register2",    3,
+            ubyte,  "mod",          2,
         ));
     }
     ubyte b;
@@ -42,26 +85,11 @@ union ModRM
 
 static assert(ModRM.sizeof == 1);
 
-enum OperandType
+struct MemoryAccess(int Size)
 {
-    Byte,
-    Word,
-    DWord,
-    QWord
-}
-
-struct MemoryAccess
-{
-    OperandType type = OperandType.DWord;
+    alias Register = Reg!Size;
     Register register;
     int offset = 0;
-
-    this(OperandType type, Register register, int offset = 0)
-    {
-        this.type = type;
-        this.register = register;
-        this.offset = offset;
-    }
 
     this(Register register, int offset = 0)
     {
@@ -152,24 +180,24 @@ struct Block
             emitRelocation!uint();
     }
 
-    void emitRegisterMemoryAccess(Register r1, MemoryAccess r2)
+    void emitRegisterMemoryAccess(Register, MemoryAccess)(Register r1, MemoryAccess r2)
     {
         if (r2.offset)
         {
             if (r2.offset.fitsIn!byte)
             {
-                this.emit(ModRM(r2.register, r1, 1));
+                this.emit(ModRM(r2.register, r1, Mode.MemoryOffset8));
                 this.emitImmediate(cast(byte)r2.offset);
             }
             else
             {
-                this.emit(ModRM(r2.register, r1, 2));
+                this.emit(ModRM(r2.register, r1, Mode.MemoryOffsetExt));
                 this.emitImmediate(r2.offset);
             }
         }
         else
         {
-            this.emit(ModRM(r2.register, r1, 0));
+            this.emit(ModRM(r2.register, r1, Mode.Memory));
         }
     }
 
@@ -182,15 +210,15 @@ struct Block
     }
 
     // Arithmetic
-    void add(Register destination, Register source)
+    void add(Reg32 destination, Reg32 source)
     {
         this.emit(0x01);
         this.emit(ModRM(destination, source));
     }
 
-    void add(Register destination, byte immediate)
+    void add(Reg8 destination, byte immediate)
     {
-        if (destination == Register.EAX)
+        if (destination == AL)
         {
             this.emit(0x04);
             this.emitImmediate(immediate);
@@ -199,14 +227,14 @@ struct Block
         {
             this.emit(0x80);
             // Write 0 to select 0x80 /0 (add r/m8, i8)
-            this.emit(ModRM(destination, cast(Register)0));
+            this.emit(ModRM(destination, Reg8(0)));
             this.emitImmediate(immediate);
         }
     }
 
-    void add(Register destination, uint immediate)
+    void add(Reg32 destination, uint immediate)
     {
-        if (destination == Register.EAX)
+        if (destination == EAX)
         {
             this.emit(0x05);
             this.emitImmediate(immediate);
@@ -215,51 +243,38 @@ struct Block
         {
             this.emit(0x81);
             // Write 0 to select 0x81 /0 (add r/m32, i32)
-            this.emit(ModRM(destination, cast(Register)0));
+            this.emit(ModRM(destination, Reg32(0)));
             this.emitImmediate(immediate);
         }
     }
 
-    void add(MemoryAccess destination, uint immediate)
+    void add(MemoryAccess!32 destination, uint immediate)
     {
-        if (destination.type == OperandType.DWord)
-        {
-            this.emit(0x81);
-            // Write 0 to select 0x81 /0 (add r/m32, i32)
-            this.emitRegisterMemoryAccess(cast(Register)0, destination);
-            this.emitImmediate(immediate);
-        }
+        this.emit(0x81);
+        // Write 0 to select 0x81 /0 (add r/m32, i32)
+        this.emitRegisterMemoryAccess(Reg32(0), destination);
+        this.emitImmediate(immediate);
     }
 
-    void add(MemoryAccess destination, byte immediate)
+    void add(MemoryAccess!8 destination, byte immediate)
     {
-        if (destination.type == OperandType.Byte)
-        {
-            this.emit(0x80);
-            // Write 0 to select 0x80 /0 (add r/m8, i8)
-            this.emitRegisterMemoryAccess(cast(Register)0, destination);
-            this.emitImmediate(cast(byte)immediate);
-        }
-        else
-            assert(false);
+        this.emit(0x80);
+        // Write 0 to select 0x80 /0 (add r/m8, i8)
+        this.emitRegisterMemoryAccess(Reg8(0), destination);
+        this.emitImmediate(immediate);
     }
 
-    void sub(MemoryAccess destination, byte immediate)
+    void sub(MemoryAccess!8 destination, byte immediate)
     {
-        if (destination.type == OperandType.Byte)
-        {
-            this.emit(0x80);
-            // Write 5 to select 0x80 /5 (sub r/m8, i8)
-            this.emitRegisterMemoryAccess(cast(Register)5, destination);
-            this.emitImmediate(cast(byte)immediate);
-        }
-        else
-            assert(false);
+        this.emit(0x80);
+        // Write 5 to select 0x80 /5 (sub r/m8, i8)
+        this.emitRegisterMemoryAccess(Reg8(5), destination);
+        this.emitImmediate(immediate);
     }
 
-    void sub(Register destination, byte immediate)
+    void sub(Reg8 destination, byte immediate)
     {
-        if (destination == Register.EAX)
+        if (destination == AL)
         {
             this.emit(0x2C);
             this.emitImmediate(immediate);
@@ -268,14 +283,14 @@ struct Block
         {
             this.emit(0x80);
             // Write 5 to select 0x80 /5 (sub r/m8, i8)
-            this.emit(ModRM(destination, cast(Register)5));
+            this.emit(ModRM(destination, Reg8(5)));
             this.emitImmediate(immediate);
         }
     }
 
-    void sub(Register destination, uint immediate)
+    void sub(Reg32 destination, uint immediate)
     {
-        if (destination == Register.EAX)
+        if (destination == EAX)
         {
             this.emit(0x2D);
             this.emitImmediate(immediate);
@@ -284,74 +299,96 @@ struct Block
         {
             this.emit(0x81);
             // Write 5 to select 0x81 /5 (sub r/m32, i32)
-            this.emit(ModRM(destination, cast(Register)5));
+            this.emit(ModRM(destination, Reg32(5)));
             this.emitImmediate(immediate);
         }
     }
 
-    void inc(Register destination)
+    void inc(Reg32 destination)
     {
         version (X86_64)
         {
             this.emitRexW();
             this.emit(0xFF);
 
-            // Write 0 to select 0xFF /0 (inc r/m8)
-            this.emit(ModRM(destination, cast(Register)0));
+            // Write 0 to select 0xFF /0 (inc r/m)
+            this.emit(ModRM(destination, Reg32(0)));
         }
         else
         {
             // inc eax -> edi
-            this.emit(0x40 + cast(ubyte)destination);
+            this.emit(0x40 + destination.index);
         }
     }
 
-    void inc(MemoryAccess destination)
+    void inc(MemoryAccess!8 destination)
     {
-        if (destination.type == OperandType.Byte)
-            this.emit(0xFE);
-        else
+        this.emit(0xFE);
+        this.emitRegisterMemoryAccess(Reg8(0), destination);
+    }
+
+    void inc(MemoryAccess!32 destination)
+    {
+        this.emit(0xFF);
+        this.emitRegisterMemoryAccess(Reg32(0), destination);
+    }
+
+    void dec(Reg32 destination)
+    {
+        version (X86_64)
+        {
+            this.emitRexW();
             this.emit(0xFF);
 
-        this.emitRegisterMemoryAccess(cast(Register)0, destination);
-    }
-
-    void dec(Register destination)
-    {
-        // dec eax -> edi
-        this.emit(0x48 + cast(ubyte)destination);
-    }
-
-    void dec(MemoryAccess destination)
-    {
-        if (destination.type == OperandType.Byte)
-            this.emit(0xFE);
+            // Write 1 to select 0xFF /1 (dec r/m)
+            this.emit(ModRM(destination, Reg32(1)));
+        }
         else
-            this.emit(0xFF);
-
-        this.emitRegisterMemoryAccess(cast(Register)1, destination);
+        {
+            // dec eax -> edi
+            this.emit(0x48 + destination.index);
+        }
     }
 
-    void xor(Register destination, Register source)
+    void dec(MemoryAccess!8 destination)
+    {
+        this.emit(0xFE);
+        this.emitRegisterMemoryAccess(Reg8(1), destination);
+    }
+
+    void dec(MemoryAccess!32 destination)
+    {
+        this.emit(0xFF);
+        this.emitRegisterMemoryAccess(Reg32(1), destination);
+    }
+
+    void xor(Reg32 destination, Reg32 source)
     {
         this.emit(0x31);
         this.emit(ModRM(destination, source));
     }
 
     // Memory
-    void push(Register register)
+    void push(Reg32 register)
     {
         // push eax -> edi
-        this.emit(0x50 + cast(ubyte)register);
+        this.emit(0x50 + register.index);
     }
 
-    void pop(Register register)
+    void pop(Reg32 register)
     {
         // pop eax -> edi
-        this.emit(0x58 + cast(ubyte)register);
+        this.emit(0x58 + register.index);
     }
 
-    void mov(Register destination, Register source)
+    void mov(Reg32 destination, Reg32 source)
+    {
+        // mov reg, reg
+        this.emit(0x8B);
+        this.emit(ModRM(source, destination));
+    }
+
+    void mov(Reg64 destination, Reg64 source)
     {
         // mov reg, reg
         this.emitRexW();
@@ -359,79 +396,76 @@ struct Block
         this.emit(ModRM(source, destination));
     }
 
-    void mov(MemoryAccess destination, Register source)
+    void mov(MemoryAccess!32 destination, Reg32 source)
     {
         // mov [reg+disp], reg
-        this.emitRexW();
         this.emit(0x89);
         this.emitRegisterMemoryAccess(source, destination);
     }
 
-    void mov(Register destination, MemoryAccess source)
+    void mov(Reg8 destination, MemoryAccess!8 source)
     {
-        if (source.type == OperandType.Byte)
-        {
-            // mov reg8, byte ptr [reg+disp]
-            this.emit(0x8A);
-            this.emitRegisterMemoryAccess(destination, source);
-        }
-        else
-        {
-            // mov reg, dword ptr [reg+disp]
-            this.emit(0x8B);
-            this.emitRegisterMemoryAccess(destination, source);            
-        }
+        // mov reg8, byte ptr [reg+disp]
+        this.emit(0x8A);
+        this.emitRegisterMemoryAccess(destination, source);
     }
 
-    void mov(Register destination, uint immediate)
+    void mov(Reg32 destination, MemoryAccess!32 source)
+    {
+        // mov reg, dword ptr [reg+disp]
+        this.emit(0x8B);
+        this.emitRegisterMemoryAccess(destination, source);
+    }
+
+    void mov(Reg32 destination, uint immediate)
     {
         // mov reg, imm32
-        this.emit(0xB8 + cast(ubyte)destination);
+        this.emit(0xB8 + destination.index);
         this.emitImmediate(immediate);
     }
 
-    void mov(Register destination, ulong immediate)
+    void mov(Reg64 destination, ulong immediate)
     {
         // mov reg, imm64
         this.emitRexW();
-        this.emit(0xB8 + cast(ubyte)destination);
+        this.emit(0xB8 + destination.index);
         this.emitImmediate(immediate);
     }
 
-    void mov(MemoryAccess destination, byte immediate)
+    void mov(MemoryAccess!8 destination, byte immediate)
     {
-        if (destination.type != OperandType.Byte)
-            assert(false);
-
         this.emit(0xC6);
         // Write 0 to select 0xC6 /0 (mov r/m8, i8)
-        this.emitRegisterMemoryAccess(cast(Register)0, destination);
-        this.emitImmediate(cast(byte)immediate);
-    }
-
-    void mov(MemoryAccess destination, uint immediate)
-    {
-        if (destination.type != OperandType.DWord)
-            assert(false);
-
-        this.emit(0xC7);
-        // Write 0 to select 0xC7 /0 (mov r/m32, i32)
-        this.emitRegisterMemoryAccess(cast(Register)0, destination);
+        this.emitRegisterMemoryAccess(Reg8(0), destination);
         this.emitImmediate(immediate);
     }
 
-    void cmp(Register source, ubyte immediate)
+    void mov(MemoryAccess!32 destination, uint immediate)
     {
-        if (source == Register.EAX)
+        this.emit(0xC7);
+        // Write 0 to select 0xC7 /0 (mov r/m32, i32)
+        this.emitRegisterMemoryAccess(Reg32(0), destination);
+        this.emitImmediate(immediate);
+    }
+
+    void cmp(Reg8 source, ubyte immediate)
+    {
+        if (source == AL)
         {
             this.emit(0x3C);
             this.emit(immediate);
         }
+        else
+        {
+            this.emit(0x80);
+            // 0x80 /7 (cmp r/m8, i8)
+            this.emit(ModRM(source, Reg8(7)));
+        }
     }
 
-    void cmp(Register source, uint immediate)
+    void cmp(Reg32 source, uint immediate)
     {
-        if (source == Register.EAX)
+        if (source == EAX)
         {
             this.emit(0x3D);
             this.emitImmediate(immediate);
@@ -440,22 +474,17 @@ struct Block
         {
             this.emit(0x81);
             // Write 7 to select 0x81 /7 (cmp r/m32, i32)
-            this.emit(ModRM(source, cast(Register)7));
+            this.emit(ModRM(source, Reg32(7)));
             this.emitImmediate(immediate);
         }
     }
 
-    void cmp(MemoryAccess destination, byte immediate)
+    void cmp(MemoryAccess!8 destination, byte immediate)
     {
-        if (destination.type == OperandType.Byte)
-        {
-            this.emit(0x80);
-            // Write 7 to select 0x80 /7 (cmp r/m8, i8)
-            this.emitRegisterMemoryAccess(cast(Register)7, destination);
-            this.emitImmediate(cast(byte)immediate);
-        }
-        else
-            assert(false);
+        this.emit(0x80);
+        // Write 7 to select 0x80 /7 (cmp r/m8, i8)
+        this.emitRegisterMemoryAccess(Reg8(7), destination);
+        this.emitImmediate(immediate);
     }
 
     // Control flow
@@ -485,11 +514,12 @@ struct Block
         this.emitGenericRelocation(destination);
     }
 
-    void call(Register destination)
+    void call(int Size)(Reg!Size destination)
+        if (Size >= 32)
     {
         this.emit(0xFF);
-        // Write 2 to select call r/m64 (0xFF /2)
-        this.emit(ModRM(destination, cast(Register)2));
+        // Write 2 to select call r/m (0xFF /2)
+        this.emit(ModRM(destination, Reg!Size(2)));
     }
 
     void label(string name)
@@ -511,14 +541,14 @@ struct Block
     }
 
     // Forwards to constructor for convenience in `with` blocks
-    MemoryAccess bytePtr(Args...)(Args args)
+    MemoryAccess!8 bytePtr(Args...)(Args args)
     {
-        return MemoryAccess(OperandType.Byte, args);
+        return MemoryAccess!8(args);
     }
 
-    MemoryAccess dwordPtr(Args...)(Args args)
+    MemoryAccess!32 dwordPtr(Args...)(Args args)
     {
-        return MemoryAccess(OperandType.DWord, args);
+        return MemoryAccess!32(args);
     }
 
     void dump()
@@ -712,13 +742,16 @@ unittest
         testBuffer ~= c;
     }
 
-    with (block) with (Register)
+    with (block)
     {
         push(EBP);
         mov(EBP, ESP);
 
         push(EBX);
-        mov(EBX, cast(size_t)&putchar_test);
+        version (X86)
+            mov(EBX, cast(size_t)&putchar_test);
+        else version (X86_64)
+            mov(RBX, cast(size_t)&putchar_test);
         xor(EAX, EAX);
 
         label("LOOP");
@@ -735,7 +768,7 @@ unittest
                 mov(EDI, ECX);
                 call(EBX);
             }
-            version (Windows)
+            else version (Windows)
             {
                 sub(ESP, 32);
                 call(EBX);
@@ -780,7 +813,7 @@ unittest
     writeln("Test: mov reg, [reg+offset]");
     Block block;
 
-    with (block) with (Register)
+    with (block)
     {
         push(EBP);
         mov(EBP, ESP);
@@ -788,7 +821,7 @@ unittest
         {
             version (Posix)
                 mov(EAX, EDI);
-            version (Windows)
+            else version (Windows)
                 mov(EAX, ECX);
         }
         else
@@ -815,7 +848,7 @@ unittest
     writeln("Test: add/sub byte ptr [reg], i8");
     Block block;
 
-    with (block) with (Register)
+    with (block)
     {
         push(EBP);
         mov(EBP, ESP);
@@ -824,22 +857,22 @@ unittest
         version (X86_64)
         {
             version (Posix)
-                mov(EDX, EDI);
-            version (Windows)
-                mov(EDX, ECX);
+                mov(RDX, RDI);
+            else version (Windows)
+                mov(RDX, RCX);
         }
         else
             mov(EDX, dwordPtr(EBP, 8));
         // array[0] += 5
-        add(bytePtr(EDX), 5);
+        add(bytePtr(DL), 5);
         // Move to array[1]
         inc(EDX);
         // array[1] += 10
-        add(bytePtr(EDX), 10);
+        add(bytePtr(DL), 10);
         // Move to array[2]
         inc(EDX);
         // array[2] -= 10
-        sub(bytePtr(EDX), 10);
+        sub(bytePtr(DL), 10);
 
         pop(EBP);
         ret;
